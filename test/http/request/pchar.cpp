b73113be702ab17c9548484590c3ba4dc73ea27a
Franck Hu <byfranckhu@outlook.com>
2023-12-23 21:03:34 +0100

    test(parser): pchar()

--- test/http/request/pchar.cpp
+++ test/http/request/pchar.cpp
@@ -29,3 +29,51 @@ TEST(pchar, space) {
 
   ASSERT_EQ(pchar_parse(&it), ParseErr);
 }
+
+TEST(pchar, unreserved) {
+  const char *actual = "3-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pchar_parse(&it), ParseOk);
+}
+
+TEST(pchar, pct_encoded) {
+  const char *actual = "%A3-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pchar_parse(&it), ParseOk);
+}
+
+TEST(pchar, sub_delims) {
+  const char *actual = "'-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pchar_parse(&it), ParseOk);
+}
+
+TEST(pchar, square_close_bracket) {
+  const char *actual = "]-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pchar_parse(&it), ParseErr);
+}

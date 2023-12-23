83e148db9b751104071b35c5711e141c3f4f9fb6
Franck Hu <byfranckhu@outlook.com>
2023-12-23 20:59:57 +0100

    test(parser): sub_delims() and pct_encoded()

---
+++ test/http/request/pct_encoded.cpp
@@ -0,0 +1,115 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "http/request/pct_encoded.h"
+#include "misc/vector/iter.h"
+#include "misc/vector/vector.h"
+}
+
+TEST(pct_encoded, empty) {
+  const char *actual = "";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pct_encoded_parse(&it), ParseErr);
+}
+
+TEST(pct_encoded, space) {
+  const char *actual = " -";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pct_encoded_parse(&it), ParseErr);
+}
+
+TEST(pct_encoded, hex) {
+  const char *actual = "%3F-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pct_encoded_parse(&it), ParseOk);
+}
+
+TEST(pct_encoded, digit) {
+  const char *actual = "%33-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pct_encoded_parse(&it), ParseOk);
+}
+
+TEST(pct_encoded, hex_min) {
+  const char *actual = "%00-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pct_encoded_parse(&it), ParseOk);
+}
+
+TEST(pct_encoded, hex_max) {
+  const char *actual = "%FF-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pct_encoded_parse(&it), ParseOk);
+}
+
+TEST(pct_encoded, not_hex) {
+  const char *actual = "%FG-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pct_encoded_parse(&it), ParseErr);
+}
+
+TEST(pct_encoded, unfinished_hex) {
+  const char *actual = "%F-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pct_encoded_parse(&it), ParseErr);
+}
+
+TEST(pct_encoded, no_percent) {
+  const char *actual = "AF-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(pct_encoded_parse(&it), ParseErr);
+}
---
+++ test/http/request/sub_delims.cpp
@@ -0,0 +1,199 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "http/request/sub_delims.h"
+#include "misc/vector/iter.h"
+#include "misc/vector/vector.h"
+}
+
+TEST(sub_delims, empty) {
+  const char *actual = "";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseErr);
+}
+
+TEST(sub_delims, space) {
+  const char *actual = " -";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseErr);
+}
+
+TEST(sub_delims, exclamation) {
+  const char *actual = "!-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, dollar) {
+  const char *actual = "$-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, ampersand) {
+  const char *actual = "&-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, single_quote) {
+  const char *actual = "'-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, round_open_bracket) {
+  const char *actual = "(-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, round_close_bracket) {
+  const char *actual = ")-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, star) {
+  const char *actual = "*-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, plus) {
+  const char *actual = "+-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, comma) {
+  const char *actual = ",-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, semi_colon) {
+  const char *actual = ";-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, equal) {
+  const char *actual = "=-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseOk);
+}
+
+TEST(sub_delims, digit) {
+  const char *actual = "3-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseErr);
+}
+
+TEST(sub_delims, alpha) {
+  const char *actual = "a-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseErr);
+}
+
+TEST(sub_delims, square_open_bracket) {
+  const char *actual = "[-";
+  struct Vec vec;
+  struct IterVec it;
+
+  vec_init(&vec);
+  vec_push_str(&vec, actual, strlen(actual));
+  iterVec_init(&it, &vec);
+
+  ASSERT_EQ(sub_delims_parse(&it), ParseErr);
+}
